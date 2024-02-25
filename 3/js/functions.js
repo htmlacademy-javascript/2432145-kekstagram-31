8dc4b460eec16346c0d025cbef75ec38429cd16a
Keks <keks@htmlacademy.ru>
2024-02-25 17:00:33 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,49 @@
+//Необходимо создать следующие функции для ДЗ module2-task1:
+/* 1. Функция для проверки длины строки:
+-входные данные: строка для проверки и длина строки для проверки.
+-result: если длина приходящей строки меньше или совпадает c length - приходит true, иначе false. */
+
+const compareLength = (input = '', length = 1) => input.length <= length;
+
+compareLength('проверяемая строка', 20);
+compareLength('проверяемая строка', 18);
+compareLength('проверяемая строка', 10);
+
+/* 2. Функция для проверки является-ли строка палиндромом:
+- Вывод: если да - true, иначе false.
+- Функция учитывает регистр и не учитывает пробелы. */
+
+function isPalindrome(input = '') {
+  const normalizeString = input.replaceAll(' ', '').toUpperCase();
+  let palindromeString = '';
+  for (let i = normalizeString.length - 1; i >= 0; i--) {
+    palindromeString += normalizeString[i];
+  }
+  return palindromeString === normalizeString;
+}
+
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
+isPalindrome('Лёша на полке клопа нашёл ');
+
+/* 3. Функция, извлекающая цифры от 0 до 9 из строки и возвращающая их в виде целого положительного числа.
+Если в строке нет чисел - возвращается NaN;  */
+
+function getNumber(input) {
+  const str = input.toString();
+  let result = '';
+  for (let i = 0; i < input.length; i++) {
+    const digit = parseInt(str[i], 10);
+    if (!Number.isNaN(digit)) {
+      result += digit;
+    }
+  }
+  return result ? parseInt(result, 10) : NaN;
+}
+
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
---
+++ 3/js/main.js
@@ -0,0 +1,44 @@
+const messages = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const names = ['Виталий', 'Артём', 'Валерий', 'Кузьма', 'Артур', 'Платон', 'Александр', 'Тимур', 'Алексей', 'Арсений', 'Олег',
+  'Герасим', 'Антон', 'Василий'];
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getNumberId = getRandomInteger(1, 25);
+const getLikesNumber = getRandomInteger(15, 200);
+const getPhotoUrl = getRandomInteger(1, 25);
+const getCommentId = getRandomInteger(Math.random(), Math.random());
+const getAvatarUrl = getRandomInteger(1, 6);
+const getCommentAmount = getRandomInteger(0, 30);
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const getObjectComment = () => ({
+  id: getCommentId,
+  avatar: `../img/avatar-${getAvatarUrl}.svg`,
+  messages: getRandomArrayElement(messages),
+  name: getRandomArrayElement(names),
+});
+
+const getPhotoDescription = () => (
+  {
+    id: getNumberId,
+    url: `../photos/${getPhotoUrl}.jpg`,
+    description: 'С рулетом на балконе',
+    likes: getLikesNumber,
+    comments: Array.from({length: getCommentAmount}, getObjectComment),
+  }
+);
+
+getPhotoDescription();
+
