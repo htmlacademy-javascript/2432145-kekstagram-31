6ee1eb4cb90f25f6ae14d2fe294768f2943d90ca
Пакри <pakri@yandex.ru>
2024-03-16 17:59:05 +0300

    Выгрузка стеша с отменой действия по умолчанию с прошлого задания.

--- js/big-picture.js
+++ js/big-picture.js
@@ -2,12 +2,13 @@ import { openBigPicture } from './modal.js';
 
 const pictureContainer = document.querySelector('.pictures');
 
-function renderBigPhoto (pictures) {
-  pictureContainer.addEventListener ('click', (evt) => {
+function renderBigPhoto(pictures) {
+  pictureContainer.addEventListener('click', (evt) => {
     const picture = evt.target.closest('a.picture[data-id]');
     if (!picture) {
       return;
     }
+    evt.preventDefault();
     const id = Number(picture.dataset.id);
     const pictureData = pictures.find((item) => item.id === id);
     openBigPicture(pictureData);
@@ -15,4 +16,4 @@ function renderBigPhoto (pictures) {
 
 }
 
-export {renderBigPhoto};
+export { renderBigPhoto };
